ea58f76a0f64da07bf2922b8b2dcad925aa4c3ae
192.168.0.1 <90830312+negativexp@users.noreply.github.com>
2022-06-15 23:21:19 +0200

    .

---
+++ WpfAppMVVMpractice/WpfAppMVVMpractice/Model/FindPrimeNumber.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace WpfAppMVVMpractice.Model
+{
+    public class FindPrimeNumber
+    {
+        public event Action ProgressChanged;
+        public event Action ResultChanged;
+
+        private string _progress = "not start";
+        private long _result = 0;
+
+        public long Result
+        {
+            get => _result;
+            set
+            {
+                _result = value;
+                ResultChanged?.Invoke();
+            }
+        }
+        public string Progress
+        {
+            get => _progress;
+            set
+            {
+                _progress = value;
+                ProgressChanged?.Invoke();
+            }
+        }
+        public async void FindPrimeNumberAsync(object o)
+        {
+            await Task.Run(_FindPrimeNumber);
+            //_FindPrimeNumber();
+        }
+        public Random _radnom { get; } = new Random();
+
+        void _FindPrimeNumber()
+        {
+            int n = 100000;
+            int count = 0;
+            long a = 2;
+            while (count < n)
+            {
+                long b = 2;
+                int prime = 1;// to check if found a prime
+                while (b * b <= a)
+                {
+                    if (a % b == 0)
+                    {
+                        prime = 0;
+                        break;
+                    }
+                    b++;
+                }
+                if (prime > 0)
+                {
+                    count++;
+                }
+                a++;
+                Progress = count.ToString();
+            }
+            Progress = count.ToString() + " (Done)";
+            Result = --a;
+            //return (--a);
+        }
+    }
+}
---
+++ WpfAppMVVMpractice/WpfAppMVVMpractice/ViewModel/MainWindowViewModel.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.ComponentModel;
+using System.Windows.Input;
+
+namespace WpfAppMVVMpractice.ViewModel
+{
+    public class MainWindowViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+        DelegateCommand StartWork;
+        public Model.FindPrimeNumber FindPrimeNumber { get; } =
+            new Model.FindPrimeNumber();
+
+        public MainWindowViewModel()
+        {
+            StartWork = new DelegateCommand(FindPrimeNumber.FindPrimeNumberAsync);
+            FindPrimeNumber.ProgressChanged += FindPrimeNumber_ProgressChanged;
+            FindPrimeNumber.ResultChanged += FindPrimeNumber_ResultChanged;
+        }
+
+        private void FindPrimeNumber_ProgressChanged()
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("Progress"));
+        }
+        private void FindPrimeNumber_ResultChanged()
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("Result"));
+        }
+
+        public ICommand Start
+        {
+            get => StartWork;
+        }
+        public string Progress
+        {
+            get => FindPrimeNumber.Progress;
+        }
+        public string Result
+        {
+            get => Convert.ToString(FindPrimeNumber.Result);
+        }
+    }
+}
