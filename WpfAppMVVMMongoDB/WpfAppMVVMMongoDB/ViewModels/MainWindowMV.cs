d749f6e171197d422f1bfaf363fbd3f519b9a1f7
192.168.0.1 <90830312+negativexp@users.noreply.github.com>
2022-06-18 16:59:34 +0200

    idk dude

--- WpfAppMVVMMongoDB/WpfAppMVVMMongoDB/ViewModels/MainWindowMV.cs
+++ WpfAppMVVMMongoDB/WpfAppMVVMMongoDB/ViewModels/MainWindowMV.cs
@@ -6,6 +6,7 @@ using System.Threading.Tasks;
 using System.ComponentModel;
 using System.Windows.Input;
 using System.Windows;
+using MongoDB.Driver;
 
 namespace WpfAppMVVMMongoDB.ViewModels
 {
@@ -13,33 +14,79 @@ namespace WpfAppMVVMMongoDB.ViewModels
     {
         //this shit needs direct access to database a
         //in order to update datagrid
+        public MongoClient _client;
+        public IMongoDatabase _database;
+        public IMongoCollection<Models.PersonModel> _collection;
 
         public event PropertyChangedEventHandler PropertyChanged;
 
-        public ICommand InsertPersonCommand;
+        public ICommand DeleteUserCommand;
+        public ICommand ListCountCommand;
 
         public Models.DatabaseModel DatabaseModel { get; } =
             new Models.DatabaseModel();
 
         public MainWindowMV()
         {
-            InsertPersonCommand = new DelegateCommand(DatabaseModel.AddPerson);
+            ConnectToDatabase();
+            Items = FetchCollection();
+            DeleteUserCommand = new DelegateCommand(RemoveDocument);
+            ListCountCommand = new DelegateCommand(ListCount);
             DatabaseModel.ItemsChanged += DatabaseModel_ItemsChanged;
         }
 
         private void DatabaseModel_ItemsChanged()
         {
-            MessageBox.Show($"count: {Items.Count}");
+            MessageBox.Show("items changed");
         }
 
+        void ListCount(object o)
+        {
+            MessageBox.Show($"Count:{Items.Count}");
+        }
+
+        void ConnectToDatabase()
+        {
+            _client = new MongoClient("mongodb://127.0.0.1:27017");
+            _database = _client.GetDatabase("testing_db");
+            _collection = _database.GetCollection<Models.PersonModel>("people");
+        }
+
+        List<Models.PersonModel> FetchCollection()
+        {
+            var results = _collection.Find(_ => true);
+            return results.ToList();
+        }
+
+        void RemoveDocument(object o)
+        {
+            if(o is Models.PersonModel person)
+            {
+                var filter = Builders<Models.PersonModel>.Filter.Eq("Id", person.Id);
+                _collection.DeleteOne(filter);
+                Items = _collection.Find(_ => true).ToList();
+                MessageBox.Show($"User removed: {person.Id}");
+            }
+        }
+
+        private List<Models.PersonModel> _items;
         public List<Models.PersonModel> Items
         {
-            get => DatabaseModel.Items;
+            get => _items;
+            set
+            {
+                _items = value;
+            }
         }
 
-        public ICommand InserPerson
+        public ICommand DeleteUser
         {
-            get => InsertPersonCommand;
+            get => DeleteUserCommand;
+        }
+
+        public ICommand Count
+        {
+            get => ListCountCommand;
         }
     }
 }
