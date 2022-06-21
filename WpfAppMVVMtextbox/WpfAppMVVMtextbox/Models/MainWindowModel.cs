1c518622ce5d670e0c63e5c2df3c8e5397a923a6
evilbitmap <matyas.schuller@educhem.cz>
2022-06-21 11:23:30 +0200

    i think i understand it more

---
+++ WpfAppMVVMtextbox/WpfAppMVVMtextbox/Models/MainWindowModel.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel;
+using System.IO;
+using System.Windows;
+
+namespace WpfAppMVVMtextbox.Models
+{
+    class MainWindowModel
+    {
+        private string _name;
+        private string _age;
+        private string _job;
+        public string Name
+        {
+            get => _name;
+            set => _name = value;
+        }
+        public string Age
+        {
+            get => _age;
+            set => _age = value;
+        }
+        public string Job
+        {
+            get => _job;
+            set => _job = value;
+        }
+
+        public void CreateFile(object o)
+        {
+            string x = $"{_name} ; {_age} ; {_job}";
+            File.WriteAllText("data.txt", x);
+            MessageBox.Show("File created");
+        }
+    }
+}
---
+++ WpfAppMVVMtextbox/WpfAppMVVMtextbox/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.ComponentModel;
+using System.Windows.Input;
+using System.IO;
+using System.Windows;
+
+namespace WpfAppMVVMtextbox.ViewModels
+{
+    class MainWindowViewModel
+    {
+        private Models.MainWindowModel MainWindowModel;
+
+        DelegateCommand WriteFileCommand;
+
+        public MainWindowViewModel()
+        {
+            MainWindowModel = new Models.MainWindowModel();
+            WriteFileCommand = new DelegateCommand(MainWindowModel.CreateFile);
+        }
+
+        public Models.MainWindowModel TextBoxes
+        {
+            get => MainWindowModel;
+            set => MainWindowModel = value;
+        }
+
+        public ICommand WriteFile
+        {
+            get => WriteFileCommand;
+        }
+    }
+}
